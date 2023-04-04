628ed19ffb4f06e0a8e8636b93dde9819872290c
ericjbanach <117881196+ericjbanach@users.noreply.github.com>
2023-04-04 16:44:16 -0400

    Fixing styles

--- js/bracketmaker.js
+++ js/bracketmaker.js
@@ -130,11 +130,11 @@ function makeBracket() {
     }
 
     //Generate html
-    generateSingleBracket(size, size, teams);
+    generateSingleBracket(size, size, teams, 0);
 }
 
-function generateSingleBracket(size, originsize, teams){
-    
+function generateSingleBracket(size, originsize, teams, count){
+
     //Get parent element
     const parent = document.getElementById("main");
 
@@ -145,73 +145,86 @@ function generateSingleBracket(size, originsize, teams){
 
     //Construct a round of games
     for(i=0; i<size; i++){
-        //Create game div and class
-        const bracketgameDiv = document.createElement("div");
-        bracketgameDiv.classList.add("bracket-game");
 
-        let teamTop = document.createTextNode("-");
-        let teamBot = document.createTextNode("-");
+      //Create spacer
+      const spacer = document.createElement("div");
+      spacer.style.paddingTop = "5px";
+      const spacer2 = document.createElement("div");
+      spacer2.style.paddingTop = "5px";
+      
+      //Create game div and class
+      const bracketgameDiv = document.createElement("div");
+      bracketgameDiv.classList.add("bracket-game");
 
-        if(size===originsize){
-            teamTop = document.createTextNode(`${teams[i]}`);
-            teamBot = document.createTextNode(`${teams[i+1]}`);
-        }
-        let scoreTop = document.createTextNode("0");
-        let scoreBot = document.createTextNode("0");
+      let teamTop = document.createTextNode("-");
+      let teamBot = document.createTextNode("-");
 
-        //Create player div and class
-        const playerTopDiv = document.createElement("div");
-        playerTopDiv.classList.add("player");
-        playerTopDiv.classList.add("top");
-        playerTopDiv.setAttribute("id", `r-of-${size}-team-${i}`);
-        playerTopDiv.setAttribute("name", `r-of-${size}-team-${i}`);
-        playerTopDiv.appendChild(teamTop);
+      if(size===originsize){
+          teamTop = document.createTextNode(`${teams[i]}`);
+          teamBot = document.createTextNode(`${teams[i+1]}`);
+      }
+      let scoreTop = document.createTextNode("0");
+      let scoreBot = document.createTextNode("0");
 
-        const playerBotDiv = document.createElement("div");
-        playerBotDiv.classList.add("player");
-        playerBotDiv.classList.add("bot");
-        playerBotDiv.setAttribute("id", `r-of-${size}-team-${i+1}`);
-        playerBotDiv.setAttribute("name", `r-of-${size}-team-${i+1}`);
-        playerBotDiv.appendChild(teamBot);
+      //Create player div and class
+      const playerTopDiv = document.createElement("div");
+      playerTopDiv.classList.add("player");
+      playerTopDiv.classList.add("top");
+      playerTopDiv.setAttribute("id", `r-of-${size}-team-${i}`);
+      playerTopDiv.setAttribute("name", `r-of-${size}-team-${i}`);
+      playerTopDiv.appendChild(teamTop);
 
-        //Create score div and class
-        const scoreTopDiv = document.createElement("input");
-        scoreTopDiv.classList.add("score");
-        scoreTopDiv.setAttribute("id", `r-of-${size}-team-${i}-score`);
-        scoreTopDiv.setAttribute("name", `r-of-${size}-team-${i}-score`);
-        scoreTopDiv.setAttribute("onChange",`determineWinner(this.value, ${size}, ${i}, ${i+1})`);
-        scoreTopDiv.setAttribute("type", "number");
-        scoreTopDiv.setAttribute("placeholder","0");
-        //scoreTopDiv.appendChild(scoreTop);
-        const scoreBotDiv = document.createElement("input");
-        scoreBotDiv.classList.add("score");
-        scoreBotDiv.setAttribute("id", `r-of-${size}-team-${i+1}-score`);
-        scoreBotDiv.setAttribute("name", `r-of-${size}-team-${i+1}-score`);
-        scoreBotDiv.setAttribute("onChange",`determineWinner(this.value, ${size}, ${i+1}, ${i})`);
-        scoreBotDiv.setAttribute("type", "number");
-        scoreBotDiv.setAttribute("placeholder","0");
-        //scoreBotDiv.appendChild(scoreBot);
 
-        if(size!==originsize){
-            roundDiv.style.marginLeft = 0;
-            let paddingSize = 12.5*(originsize/size);
-            bracketgameDiv.style.paddingTop = `${paddingSize}px`;
-            bracketgameDiv.style.paddingBottom = `${paddingSize}px`;
-        }
+      const playerBotDiv = document.createElement("div");
+      playerBotDiv.classList.add("player");
+      playerBotDiv.classList.add("bot");
+      playerBotDiv.setAttribute("id", `r-of-${size}-team-${i+1}`);
+      playerBotDiv.setAttribute("name", `r-of-${size}-team-${i+1}`);
+      playerBotDiv.appendChild(teamBot);
 
-        //Add the score div to the player div
-        playerTopDiv.appendChild(scoreTopDiv);
-        playerBotDiv.appendChild(scoreBotDiv);
+      //Create score div and class
+      const scoreTopDiv = document.createElement("input");
+      scoreTopDiv.classList.add("score");
+      scoreTopDiv.setAttribute("id", `r-of-${size}-team-${i}-score`);
+      scoreTopDiv.setAttribute("name", `r-of-${size}-team-${i}-score`);
+      scoreTopDiv.setAttribute("onChange",`determineWinner(this.value, ${size}, ${i}, ${i+1})`);
+      scoreTopDiv.setAttribute("type", "number");
+      scoreTopDiv.setAttribute("placeholder","0");
 
-        //Add top class to player and add to game div
-        bracketgameDiv.appendChild(playerTopDiv);
+    
+      const scoreBotDiv = document.createElement("input");
+      scoreBotDiv.classList.add("score");
+      scoreBotDiv.setAttribute("id", `r-of-${size}-team-${i+1}-score`);
+      scoreBotDiv.setAttribute("name", `r-of-${size}-team-${i+1}-score`);
+      scoreBotDiv.setAttribute("onChange",`determineWinner(this.value, ${size}, ${i+1}, ${i})`);
+      scoreBotDiv.setAttribute("type", "number");
+      scoreBotDiv.setAttribute("placeholder","0");
 
-        //Add bot class to player and add another to game div
-        bracketgameDiv.appendChild(playerBotDiv);
 
-        //Add the game div to the round div
-        roundDiv.appendChild(bracketgameDiv);
-        i++;
+      if(size!==originsize ){
+          roundDiv.appendChild(spacer);
+          roundDiv.style.marginLeft = 0;
+          let paddingSize = Math.round((26.4758*Math.pow(Math.E, 0.697574*count))-26.2651);
+          bracketgameDiv.style.paddingTop = `${paddingSize}px`;
+          bracketgameDiv.style.paddingBottom = `${paddingSize}px`;
+      }
+
+      //Add the score div to the player div
+      playerTopDiv.appendChild(scoreTopDiv);
+      playerBotDiv.appendChild(scoreBotDiv);
+
+      //Add top class to player and add to game div
+      bracketgameDiv.appendChild(playerTopDiv);
+
+      //Add bot class to player and add another to game div
+      bracketgameDiv.appendChild(playerBotDiv);
+
+      //Add the game div to the round div
+      roundDiv.appendChild(bracketgameDiv);
+      if(size!==originsize ){
+        roundDiv.appendChild(spacer2);
+      }
+      i++;
     }
 
     //Add the round to the parent
@@ -223,40 +236,91 @@ function generateSingleBracket(size, originsize, teams){
 
         //code for lines goes here
         for(i=0; i<size/4; i++){
-            const topLine = document.createElement("div");
-            topLine.classList.add("top-line");
+          const topLine = document.createElement("div");
+          topLine.classList.add("top-line");
 
-            const botLine = document.createElement("div");
-            botLine.classList.add("bottom-line");
+          const botLine = document.createElement("div");
+          botLine.classList.add("bottom-line");
 
-            const vertLine = document.createElement("div");
-            vertLine.classList.add("vert-line");
+          const nextLine = document.createElement("div");
+          nextLine.classList.add("next-line");
 
-            const nextLine = document.createElement("div");
-            nextLine.classList.add("next-line");
+          if(i===0){
+            let marginSize = Math.round((26.6131*Math.pow(Math.E, 0.694033*count))+.500125);
+            topLine.style.marginTop = `${marginSize}px`;
+            topLine.style.marginBottom = `${marginSize}px`;
 
-            const clear1 = document.createElement("div");
-            clear1.classList.add("clear");
-            const clear2 = document.createElement("div");
-            clear2.classList.add("clear");
-            const clear3 = document.createElement("div");
-            clear3.classList.add("clear");
-            const clear4 = document.createElement("div");
-            clear4.classList.add("clear");
+            marginSize = Math.round((55.802*Math.pow(Math.E, .683042*count))-5.02787);
+            botLine.style.marginTop = `${marginSize}px`;
+            botLine.style.marginBottom = `${marginSize}px`
+          }
+          else{
+            let marginSize = Math.round((54.937*Math.pow(Math.E, 0.68665*count))-3.01247);
+            topLine.style.marginTop = `${marginSize}px`;
+            topLine.style.marginBottom = `${marginSize}px`
 
-            connectDiv.appendChild(topLine);
-            //connectDiv.appendChild(clear1);
-            connectDiv.appendChild(botLine);
-           // connectDiv.appendChild(clear2);
-            connectDiv.appendChild(vertLine);
-            //connectDiv.appendChild(clear3);
-            connectDiv.appendChild(nextLine);
-            //connectDiv.appendChild(clear4);
+            marginSize = Math.round((55.802*Math.pow(Math.E, .683042*count))-5.02787);
+            botLine.style.marginTop = `${marginSize}px`;
+            botLine.style.marginBottom = `${marginSize}px`
+          }
+
+          connectDiv.appendChild(topLine);
+          connectDiv.appendChild(botLine);
+
+        }
+
+        const connectDiv2 = document.createElement("div");
+        connectDiv2.classList.add("connectors");
+        connectDiv2.style.minWidth = "2px";
+
+        //code for lines goes here
+        for(i=0; i<size/4; i++){
+          const vertLine = document.createElement("div");
+          vertLine.classList.add("vert-line");
+
+          connectDiv2.appendChild(vertLine);
+        }
+
+        const connectDiv3 = document.createElement("div");
+        connectDiv3.classList.add("connectors");
+
+        //code for lines goes here
+        for(i=0; i<size/8; i++){
+          const topLine = document.createElement("div");
+          topLine.classList.add("top-line");
+
+          const botLine = document.createElement("div");
+          botLine.classList.add("bottom-line");
+
+          if(i===0){
+            let marginSize = Math.round((26.6131*Math.pow(Math.E, 0.694033*(count+1)))+.500125);
+            topLine.style.marginTop = `${marginSize}px`;
+            topLine.style.marginBottom = `${marginSize}px`;
+
+            marginSize = Math.round((55.802*Math.pow(Math.E, .683042*(count+1)))-5.02787);
+            botLine.style.marginTop = `${marginSize}px`;
+            botLine.style.marginBottom = `${marginSize}px`
+          }
+          else{
+            let marginSize = Math.round((54.937*Math.pow(Math.E, 0.68665*(count+1)))-3.01247);
+            topLine.style.marginTop = `${marginSize}px`;
+            topLine.style.marginBottom = `${marginSize}px`
+
+            marginSize = Math.round((55.802*Math.pow(Math.E, .683042*(count+1)))-5.02787);
+            botLine.style.marginTop = `${marginSize}px`;
+            botLine.style.marginBottom = `${marginSize}px`
+          }
+
+          connectDiv3.appendChild(topLine);
+          connectDiv3.appendChild(botLine);
         }
 
         parent.append(connectDiv);
+        parent.append(connectDiv2);
+        parent.append(connectDiv3);
 
-        generateSingleBracket(size/2, originsize, teams);
+        count++;
+        generateSingleBracket(size/2, originsize, teams, count);
     }
 
 }
